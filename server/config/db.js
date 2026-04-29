2427ba090265c58d43c22bd42b5857e262248780
NewUserName <ramanpal7228@gmail.com>
2026-04-29 18:36:03 +0530

    updated

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const mongoURI = process.env.MONGO_URI
+            ?.replace('${dbuser}', process.env.dbuser)
+            ?.replace('${dbpass}', process.env.dbpass);
+
+        const conn = await mongoose.connect(mongoURI || process.env.MONGO_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
