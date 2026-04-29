39a32454fb71e6cb1cbf7e51cf154320bbc97e49
NewUserName <ramanpal7228@gmail.com>
2026-04-29 22:51:26 +0530

    updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['axios', 'recharts']
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          charts: ['recharts']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
+  }
 })
